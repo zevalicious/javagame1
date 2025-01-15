aa296211f45340b9817bd9090339dd7082b991aa
zev <150071728+zevalicious@users.noreply.github.com>
2025-01-15 11:50:13 -0700

    Create fileName.java

---
+++ fileName.java
@@ -0,0 +1,26 @@
+public class fileName {
+    public static void main(String[] args) {
+
+
+
+        int [] numbers = {1 , 2 , 4 , 9 , 3};
+        int sum = 0;
+for ( int individual : numbers){
+sum += individual;
+}
+double count = (double) numbers.length;
+int average = sum/(int)count;
+int thingIlookFor = 42;
+for (int num:numbers)
+{
+	if(num == thingIlookFor){
+	int temp = num;
+}
+}
+
+
+
+
+    }
+
+}
