b838029af8ba93977adaf4c4d4516d736c2a4c2f
zev <150071728+zevalicious@users.noreply.github.com>
2025-01-29 11:49:01 -0700

    Update Dave.java

--- Dave.java
+++ Dave.java
@@ -1,14 +1,16 @@
+import java.util.ArrayList;
 
 public class Dave {
+    int health;
     public int die(){ //method, thing that the class can do
     System.out.println("");
      return 0;
     }
-    public int heal(int health){ //method, thing that the class can do
+    public int heal(){ //method, thing that the class can do
         health ++;
        return health;
     }
-    public int fight(int health){ //method, thing that the class can do
+    public int fight(){ //method, thing that the class can do
         double y = Math.random();
         if (y >= 0.5)
         {
@@ -24,5 +26,15 @@ public class Dave {
     }
 
     public static void main(String[] args) {
+                ArrayList<Integer> myList = new ArrayList<Integer>();
+        myList.add(50);
+        myList.add(30);
+        myList.add(20);
+        int total = 0;
+        for (Integer value : myList)
+        {
+            total += value;
+        }
+        System.out.println("Sum of all elements: " + total);
 }
 }
