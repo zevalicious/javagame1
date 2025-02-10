9a85772f6123e2d8995e907f967a797f3da9ee81
zev <150071728+zevalicious@users.noreply.github.com>
2025-02-10 12:21:04 -0700

    fixed roomjava made it
    uhh

---
+++ Room.java
@@ -0,0 +1,33 @@
+public class Room {
+    public int roomNum;
+    public boolean canMoveLeft;
+    public boolean canMoveRight;
+    public boolean canMoveUp;
+    public boolean canMoveDown;
+
+    public void setRoomMoves(boolean left, boolean right, boolean up, boolean down){
+
+
+    }
+    public boolean canMove(int direction){
+       // if (direction == 1) && (canMoveLeft ==false){ 
+       //&& bropken
+
+
+       // }
+        else if (direction == 2){
+
+        }
+
+        else if (direction == 3){
+
+        }
+
+        else if (direction == 4){
+
+        }
+
+        return true;
+}
+}
+
