430bd767c8a3737ef2238565c5240527939f573f
zev <150071728+zevalicious@users.noreply.github.com>
2025-03-12 12:47:50 -0700

    Update MainGame.java

--- MainGame.java
+++ MainGame.java
@@ -4,12 +4,30 @@ public class MainGame {
 public boolean canGo;
 public boolean right;
 public int[] roomNumArray = { 1, 100, 2, 200, 3, 300, 4, 400, 5 , 500 };   
+public static void main(String[] args) {
 Room room1 = new Room();
-Room room2 = new Room(); 
-Room room3 = new Room(); 
-Room room4 = new Room(); 
-Room room5 = new Room(); 
+PuzzleChoice puzzle1 = new PuzzleChoice();
 
+room1.setRoomMoves(true,true,true,true);
+Room room2 = new Room(); 
+room2.setRoomMoves(true,true,true,true);
+Room room3 = new Room(); 
+room3.setRoomMoves(true,true,true,true);
+Room room4 = new Room(); 
+room4.setRoomMoves(true,true,true,true);
+Room room5 = new Room(); 
+room5.setRoomMoves(true,true,true,true);
+Room room100 = new Room();
+room100.setRoomMoves(true,true,true,true);
+Room room200 = new Room(); 
+room200.setRoomMoves(true,true,true,true);
+Room room300 = new Room(); 
+room300.setRoomMoves(true,true,true,true);
+Room room400 = new Room(); 
+room400.setRoomMoves(true,true,true,true);
+Room room500 = new Room(); 
+room500.setRoomMoves(true,true,true,true);
+}
 
     //initialize all the rooms
     //initialize all the puzzles
